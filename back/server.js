a86ec54ae0f551ca5ee91f20cdce0d995a8de823
Aaron <aarondivi@gmail.com>
2023-09-12 13:49:07 +0200

    init

--- back/server.js
+++ back/server.js
@@ -52,6 +52,6 @@ const server = http.createServer((req, res) => {
   res.end("hello world!")
 }) 
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = process.env.PORT || '3000';
 
 server.listen(port, () => console.log('Le serveur à démarré au port ' + port));
