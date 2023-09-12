927dbb537ccec238f33c9ae6d2cd5f1a4c7f0402
Aaron <aarondivi@gmail.com>
2023-09-12 13:10:13 +0200

    init

--- back/server.js
+++ back/server.js
@@ -10,36 +10,39 @@ const normalizePort = val => {
   if (port >= 0) {
     return port;
   }
-  return false;
+return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const errorHandler = error => {
-  if (error.syscall !== 'listen') {
+if (error.syscall !== 'listen') {
+  throw error;
+}
+const address = server.address();
+const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+switch (error.code) {
+  case 'EACCES':
+    console.error(bind + ' requires elevated privileges.');
+    process.exit(1);
+    break;
+  case 'EADDRINUSE':
+    console.error(bind + ' is already in use.');
+    process.exit(1);
+    break;
+  default:
     throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-    default:
-      throw error;
-  }
+}
 };
 
 const server = http.createServer(app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+const address = server.address();
+const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+console.log('Listening on ' + bind);
 });
 
-server.listen(port);
+app.listen(port, () => console.log('Le serveur à démarré au port ' + port));
